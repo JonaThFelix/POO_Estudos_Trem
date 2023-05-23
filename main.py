753ba1b9f0bd8622f53e25defbb9705c898eff31
Jonathan Felix <123984244+JonaThFelix@users.noreply.github.com>
2023-05-22 23:03:12 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from Trem import Trem
+
+#Criação do Objeto
+trem1 = Trem('Trem 101','Destino Camaragibe')
+trem2 = Trem('Trem 404','Destino Jaboatão')
+
+trem1.apresentar()
+print('-'*80,'\n')
+
+#Execução do Objeto trem1
+
+trem1.estacoes(10)
+trem1.estacoes(40)
+trem1.estacoes(100)
+
+print('-'*80,'\n')
+
+#Execução do Objeto trem2
+trem2.apresentar()
+trem2.estacoes(80)
+trem2.estacoes(10)
+trem2.estacoes(10)
+trem2.estacoes(10)
+
+
+
+
